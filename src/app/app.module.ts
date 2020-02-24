2a658c00612aa2f9d7536b9ef879fe5fec1bd49a
ecollazodominguez <ecollazodominguez@danielcastelao.org>
2020-02-24 15:55:49 +0100

    Lista fija a comprar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListacompraComponent } from './listacompra/listacompra.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListacompraComponent
   ],
   imports: [
     BrowserModule
